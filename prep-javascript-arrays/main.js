fe0efb360e942c22a7e66e47ecde695e3edf10e8
Adrian Guzman <adrian1217guzman@gmail.com>
2022-06-15 01:53:10 +0000

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,17 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'
+];
+console.log('languages array', languages);
+const firstElement = 'html';
+console.log('the first element of the array is', firstElement);
+const thirdElement = 'javascript';
+console.log('the third element of the array is', thirdElement);
+var length = 6;
+console.log('the length of the array is', length);
+const lastElement = 'postgres';
+console.log('the last element of the array is', lastElement);
