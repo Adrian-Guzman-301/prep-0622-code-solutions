a001b6add643ff3c930ca7601c9faca91ee10793
Adrian Guzman <adrian1217guzman@gmail.com>
2022-06-18 00:04:16 +0000

    Completed prep-javascript-for-loops exercise.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,15 @@
+// create your loops here.
+// for (statement 1; statement 2; statement 3) {
+// code block to be executed
+//  }
+for (var i = 1; i <= 9; i++) {
+  console.log(i);
+}
+
+for (var x = 2; x < 20; x += 2) {
+  console.log(x);
+}
+
+for (var y = 100; y > 0; y--) {
+  console.log('Time till explosion', y);
+}
